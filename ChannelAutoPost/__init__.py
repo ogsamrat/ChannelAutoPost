e6d1a0df70080d31557fa739f7b08a8d28a47e23
Dʀɪᴄᴋᴡᴏʀʟᴅ <69191476+EmiliaDevs@users.noreply.github.com>
2021-03-26 18:39:36 +0530

    [__init__] seperate user and bot clients

--- ChannelAutoPost/__init__.py
+++ ChannelAutoPost/__init__.py
@@ -3,13 +3,14 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 
-if not Config.USE_AS_USERBOT:
-    if Config.BOT_TOKEN is None or Config.APP_ID is None or Config.APP_HASH is None:
-        print("Important env variables missing... quitting!")   
-        quit(1) 
-    ChannelAutoPost = TelegramClient("CAPBOT", Config.APP_ID, Config.APP_HASH)
-else:
-    if Config.SESSION is None or Config.APP_ID is None or Config.APP_HASH is None:
-        print("Important env variables missing... quitting!")   
-        quit(1)     
-    ChannelAutoPost = TelegramClient(StringSession(Config.SESSION), Config.APP_ID, Config.APP_HASH)
+if Config.BOT_TOKEN is None or Config.APP_ID is None or Config.APP_HASH is None:
+    print("Important env variables missing... quitting!")   
+    quit(1) 
+    
+ChannelAutoPost = TelegramClient("CAPBOT", Config.APP_ID, Config.APP_HASH)
+ChannelAutoPostUB = None
+
+if Config.SESSION:
+    ChannelAutoPostUB = TelegramClient(StringSession(Config.SESSION), Config.APP_ID, Config.APP_HASH)
+    
+channel_poster = ChannelAutoPost if not ChannelAutoPostUB else ChannelAutoPostUB     
