6597a2242e37f4cd26944b83da023117e753c914
Dʀɪᴄᴋᴡᴏʀʟᴅ <69191476+EmiliaDevs@users.noreply.github.com>
2021-03-26 12:30:04 +0530

    [__init__] fix user client

--- ChannelAutoPost/__init__.py
+++ ChannelAutoPost/__init__.py
@@ -1,15 +1,15 @@
 from .Config import Config
 from telethon import TelegramClient
-from telethon.sync import TelegramClient as StringSessionMaker
 from telethon.sessions import StringSession
 
 
-if Config.USE_AS_USERBOT:
+if not Config.USE_AS_USERBOT:
     if Config.BOT_TOKEN is None or Config.APP_ID is None or Config.APP_HASH is None:
         print("Important env variables missing... quitting!")   
         quit(1) 
     ChannelAutoPost = TelegramClient("CAPBOT", Config.APP_ID, Config.APP_HASH)
 else:
-    with StringSessionMaker(StringSession(), Config.APP_ID, Config.APP_HASH) as autopost:
-        session = autopost.session.save()
-    ChannelAutoPost = TelegramClient(StringSession(session), Config.APP_ID, Config.APP_HASH)
+    if Config.SESSION is None or Config.APP_ID is None or Config.APP_HASH is None:
+        print("Important env variables missing... quitting!")   
+        quit(1)     
+    ChannelAutoPost = TelegramClient(Config.SESSION, Config.APP_ID, Config.APP_HASH)
